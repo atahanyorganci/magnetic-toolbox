303e3319e0d567dc685ca9e0da31a2e55acaa99c
Atahan Yorgancı <atahanyorganci@hotmail.com>
2020-08-31 17:29:33 +0300

    initial commit

---
+++ validators/isColinear.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = isColinear(inputArg1,inputArg2)
+%ISCOLINEAR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
